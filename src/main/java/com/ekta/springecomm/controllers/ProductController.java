86576574568df57e86648b5d3a5e4b991374b14b
Ekta3009 <ektachoudhary309@gmail.com>
2025-02-26 17:07:10 +0530

    Get Product By ID added and date format changed

--- src/main/java/com/ekta/springecomm/controllers/ProductController.java
+++ src/main/java/com/ekta/springecomm/controllers/ProductController.java
@@ -3,9 +3,7 @@ package com.ekta.springecomm.controllers;
 import com.ekta.springecomm.models.Product;
 import com.ekta.springecomm.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getAllProducts(){
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProductById(@PathVariable int id){
+        return productService.getProductById(id);
     }
 }
--- src/main/java/com/ekta/springecomm/models/Product.java
+++ src/main/java/com/ekta/springecomm/models/Product.java
@@ -1,6 +1,9 @@
 package com.ekta.springecomm.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,11 +19,13 @@ import java.util.Date;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
     private String category;
     private BigDecimal price;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "DD-MM-YYYY")
     private Date releaseDate;
     private boolean availability;
     private int quantity;
